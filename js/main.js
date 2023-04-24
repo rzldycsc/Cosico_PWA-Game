7552f6fa81e56286cb32763742fd7d89d26a0c52
rzldycsc <0319-0944@lspu.edu.ph>
2023-04-24 14:04:43 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -92,15 +92,6 @@ function selectTile() {
             errors += 1;
             document.getElementById("errors").innerText = errors;
         }
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', function() {
-              navigator.serviceWorker.register('/sw.js').then(function(registration) {
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-              }, function(err) {
-                console.log('ServiceWorker registration failed: ', err);
-              });
-            });
-          }
     }
 }
 
